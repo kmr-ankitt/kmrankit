7114deaed4728840a1f0b7953ac332f5aa4b583d
ankit <sahuankit.murhu@gmail.com>
2024-12-19 22:07:17 +0530

    Navbar done

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
+import { useRouter } from "next/navigation";
+
+const routes = [
+  {
+    name: "Home",
+    route: "/",
+  },
+  {
+    name: "Projects",
+    route: "/projects",
+  },
+];
+
+export default function Header() {
+  const router = useRouter();
+  
+  function isKeyPressed() {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "h") {
+        e.preventDefault();
+        router.push("/");
+      } else if (e.key === "p") {
+        e.preventDefault();
+        router.push("/projects");
+      }
+    });
+  }
+  isKeyPressed();
+
+  return (
+    <div className="flex items-center justify-between p-5">
+      <div className="flex items-center gap-4">
+        {routes.map((item, index) => (
+          <Link href={item.route} key={index}>
+            {"[" + item.name[0] + "] " + item.name}
+          </Link>
+        ))}
+      </div>
+      <ModeToggle />
+    </div>
+  );
+}
