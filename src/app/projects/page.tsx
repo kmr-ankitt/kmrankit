ea3c2d0afbb183603db938831882bc9c7cde9f56
ankit <sahuankit.murhu@gmail.com>
2024-12-20 23:53:51 +0530

    Width issue final fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import ProjectList from "@/components/projects/ProjectList";
 export default function page() {
   return (
     <div className="h-full m-7 lowercase mt-10 ">
-      <div className="w-screen">
+      <div className="md:w-screen max-w-screen-lg mx-auto">
         <h1 className="text-2xl font-bold">
           <span className="text-[#ff6b35]">{">"}</span> my works
         </h1>
