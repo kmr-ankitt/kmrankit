5c84dcd78b1148f228a5f6b2d14b6bc44a4ff36e
ankit <sahuankit.murhu@gmail.com>
2025-01-04 21:06:28 +0530

    Github redirect link

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,7 @@
 import ProjectList from "@/components/projects/ProjectList";
 import HyperText from "@/components/ui/hyper-text";
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
 
 export default function page() {
   return (
@@ -15,6 +17,17 @@ export default function page() {
         </HyperText>
       </div>
       <ProjectList />
+      <div className="flex items-center gap-2">
+        <Link
+          href={"https://www.github.com/kmr-ankitt"}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center group hover:underline hover:underline-offset-4 gap-2 hover:text-[#ff6b35] transition-all duration-300"
+        >
+          more on github
+          <ArrowUpRight className="w-5 h-5 transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-1" />
+        </Link>
+      </div>
     </div>
   );
 }
